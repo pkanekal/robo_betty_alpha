335e27aefdfe59fad8f04a2da93e7d298b7bda0a
Pavan Kanekal <pkanekal@gmail.com>
2016-01-11 20:36:17 -0800

    hardcoded mongoURL

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,7 @@
 /**
  * Config var for app
 **/
+process.env.MONGODB_URL = 'mongodb://heroku_1zm72tdd:ppeo8mhp9ao849s1g639rm32lv@ds043605.mongolab.com:43605/heroku_1zm72tdd';
 module.exports = {
   mongoDBUrl: process.env.MONGODB_URL || 'mongodb://localhost:27017/test',
   port: process.env.PORT || 4941,
